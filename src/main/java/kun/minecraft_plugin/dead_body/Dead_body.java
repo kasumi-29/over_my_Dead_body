288b5d9076e756f7fc95a66d79ad56d013b1976c
BuildTools <unconfigured@null.spigotmc.org>
2020-08-28 12:53:49 +0900

    コマンド /info-locate を追加 - datFilecheck メソッドの動作を修正 - 安全地帯の上限と下限を設定

--- src/main/java/kun/minecraft_plugin/dead_body/Dead_body.java
+++ src/main/java/kun/minecraft_plugin/dead_body/Dead_body.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,8 +16,8 @@ import java.util.*;
 public final class Dead_body extends JavaPlugin {
     private Location first_sp=null;
     private int big=3;
-    private String death_path="plugins\\dead_body\\deathLoc.dat";
-    private String safe_path="plugins\\dead_body\\safeLoc.dat";
+    private String death_path="plugins\\over_my_Dead_body\\deathLoc.dat";
+    private String safe_path="plugins\\over_my_Dead_body\\safeLoc.dat";
     private HashSet<XZLocation> deathLoc;
     private HashSet<XZLocation> safeZone;
     static private class zeroArray implements TabCompleter {
@@ -32,9 +33,12 @@ public final class Dead_body extends JavaPlugin {
         safeZone =new HashSet<>();
 
         saveDefaultConfig();
-        big=getConfig().getInt("big",big);
-        death_path=getConfig().getString("death_path",death_path);
-        safe_path=getConfig().getString("safe_path",safe_path);
+        big=getConfig().getInt("default_safeZone");
+        death_path=getConfig().getString("death_path");
+        safe_path=getConfig().getString("safe_path");
+
+        big=Math.max(big, 0);
+        big=Math.min(big,16);
 
         getServer().getPluginManager().registerEvents(new room_in(this), this);
         Objects.requireNonNull(getCommand("set-safezone")).setExecutor((sender, command, label, args) -> {
@@ -56,17 +60,44 @@ public final class Dead_body extends JavaPlugin {
             sender.sendMessage("Load完了......");
             return true;
         });
+        Objects.requireNonNull(getCommand("info-locate")).setExecutor((sender, command, label, args) -> {
+            ArrayList<String> msgList=info();
+            if(!(sender instanceof ConsoleCommandSender)){
+                for (String msg:msgList) {
+                    sender.sendMessage(msg);
+                }
+            }
+            for (String msg:msgList) {
+                getLogger().info(msg);
+            }
+            return true;
+        });
         Objects.requireNonNull(getCommand("set-safezone")).setTabCompleter(new zeroArray());
         Objects.requireNonNull(getCommand("save-locate")).setTabCompleter(new zeroArray());
         Objects.requireNonNull(getCommand("load-locate")).setTabCompleter(new zeroArray());
+        Objects.requireNonNull(getCommand("info-locate")).setTabCompleter(new zeroArray());
         sl_comp(false);
 
         if (safeZone.size()!=0){
             Iterator<XZLocation> iterator=safeZone.iterator();
             first_sp=Objects.requireNonNull(getServer().getWorld(iterator.next().getWorldUid())).getSpawnLocation();
         }
-
+        for (String msg:info()) {
+            getLogger().info(msg);
+        }
         getLogger().info("読み込み完了！");
+    }
+
+    public ArrayList<String> info(){
+        ArrayList<String> output=new ArrayList<>();
+        if(first_sp!=null){
+            output.add("初期スポーンは、x="+((int)first_sp.getX())+",y=?,z="+((int)first_sp.getZ())+"です");
+        }else{
+            output.add("初期スポーンは、まだ決定していません");
+        }
+        output.add("危険地帯："+deathLoc.size()+"箇所");
+        output.add("安全地帯："+safeZone.size()+"箇所");
+        return output;
     }
 
     @Override
@@ -76,16 +107,18 @@ public final class Dead_body extends JavaPlugin {
 
     private void sl_comp(boolean flag){
         if(flag){//save側
-            savedeath(death_path,deathLoc);
-            savedeath(safe_path,safeZone);
+            saveDeath(death_path,deathLoc);
+            saveDeath(safe_path,safeZone);
         }else{//load側
-            HashSet<XZLocation> tempD=loaddeath(death_path);
-            HashSet<XZLocation> tempS=loaddeath(safe_path);
+            HashSet<XZLocation> tempD= loadDeath(death_path);
+            HashSet<XZLocation> tempS= loadDeath(safe_path);
             if(tempD!=null) {
                 deathLoc = tempD;
+                getLogger().info("危険地帯をファイルから正常に読み込みました。");
             }
             if(tempS!=null) {
                 safeZone = tempS;
+                getLogger().info("安全地帯をファイルから正常に読み込みました。");
             }
         }
     }
@@ -115,7 +148,10 @@ public final class Dead_body extends JavaPlugin {
             for(int ox=0; ox<n; ox++) {
                 for (int oz=0; oz<n; oz++) {
                     l.getBlock().setType(Material.BEDROCK);
-                    if(checkP(ox,oz,n)){
+                    XZLocation nowXZl=new XZLocation(l);
+                    safeZone.add(nowXZl);
+                    deathLoc.remove(nowXZl);
+                    if(checkP(ox,oz,n)&&(n>1)){
                         getStepY(l,1).getBlock().setType(Material.BEDROCK);
                     }else {
                         getStepY(l, 1).getBlock().setType(Material.AIR);
@@ -156,7 +192,7 @@ public final class Dead_body extends JavaPlugin {
         boolean output = deathLog.exists();
         if(flag&&(!output)){
             try {
-                boolean a=new File("plugins\\dead_body").mkdirs();
+                boolean a=deathLog.getParentFile().mkdirs();
                 output=deathLog.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -164,7 +200,7 @@ public final class Dead_body extends JavaPlugin {
         }
         return !output;
     }
-    public void savedeath(String save_path,HashSet<XZLocation> savedata){
+    public void saveDeath(String save_path, HashSet<XZLocation> save_data){
         if(datFilecheck(true,save_path)){
             getLogger().info("ファイルをセーブするのに失敗しました。");
             return;
@@ -172,14 +208,14 @@ public final class Dead_body extends JavaPlugin {
         try(FileOutputStream f = new FileOutputStream(save_path);
             BufferedOutputStream b = new BufferedOutputStream(f);
             ObjectOutputStream out = new ObjectOutputStream(b)){
-            out.writeObject(savedata);
+            out.writeObject(save_data);
         } catch ( IOException e ) {
             e.printStackTrace();
         }
     }
 
     @SuppressWarnings("unchecked")
-    public HashSet<XZLocation> loaddeath(String load_path){
+    public HashSet<XZLocation> loadDeath(String load_path){
         if (datFilecheck(false,load_path)){return null;}
         try(FileInputStream f = new FileInputStream(load_path);
             BufferedInputStream b = new BufferedInputStream(f);
